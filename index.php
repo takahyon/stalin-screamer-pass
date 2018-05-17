dd776f0eb2dc4c3414a9e9b0556d763aceff6932
Takamasa Iijima <takahyon@gmail.com>
2018-05-17 21:57:44 +0900

    [add] main function based on (How many days went since stalins days)

--- index.php
+++ index.php
@@ -3,18 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <title>StalinScreamerReleaser</title>
+    <?php
+    date_default_timezone_set('Asia/Tokyo');
+
+    function time_diff($time_from, $time_to)
+    {
+        // get the diff from unit of the day
+        $dif = $time_to - $time_from;
+
+        // get diff days
+        $dif_days = (strtotime(date("Y-m-d", $dif)) - strtotime("1970-01-01")) / 86400;
+        return "{$dif_days}days";
+        echo $dif;
+    }
+
+    ?>
 </head>
 <body>
 
 <h1>StalinScreamerReleaser</h1>
 
 <?php
+//day which accesess this site
+$today = date("Y-m-d");
 
-$today = date("Y/m/d");
+$from = strtotime("1922-12-30");
+$to = strtotime($today);
 
-print($today);
+print ("Today is");
+print("<h2>$today</h2>");
+print ("Release Passcode is ");
+//print the result
+$result = time_diff($from, $to);
+print("<h1>$result</h1>");
+
 ?>
-
 
 </body>
 </html>
