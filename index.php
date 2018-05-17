4a4177d11c8566b22823a59bb31997face0c55c1
Takamasa Iijima <takahyon@gmail.com>
2018-05-17 20:24:51 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>StalinScreamerReleaser</title>
+</head>
+<body>
+
+<h1>StalinScreamerReleaser</h1>
+
+<?php
+
+$today = date("Y/m/d");
+
+print($today);
+?>
+
+
+</body>
+</html>
