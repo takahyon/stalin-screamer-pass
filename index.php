53375d98d2b852b59bcb00ff7f4c17c446c4ca16
Takamasa Iijima <takahyon@gmail.com>
2018-05-17 21:40:20 +0900

    [add]calculate each dates number

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
+<!-- Takamasa Iijima-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>StalinScreamerReleaser</title>
+    <?php
+    date_default_timezone_set('Asia/Tokyo');
+
+    function getResult($now){
+        $Y = (int)date("Y");
+        $m = (int)date("m");
+        $d = (int)date("d");
+
+        $stalin ="1922.12.30";
+        $result = $now - (double)("1922.12.30");
+        print($now);
+        return $result;
+    }
+
+    ?>
+
 </head>
 <body>
 
@@ -10,9 +28,15 @@
 
 <?php
 
-$today = date("Y/m/d");
+#date which open website
+$today = date("Y.m.d");
+$doubletoday = (double)$today;
+$pass = getResult($doubletoday);
+print ("Today is");
+print("<h2>$today</h2>");
+print ("Release Passcode is ")
+print("<h1>$pass</h1>");
 
-print($today);
 ?>
 
 
